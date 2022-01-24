ddddc55950246578c043961a5594b281de5efbb0
Chloe <chloe@Chloeui-MacBookAir.local>
2022-01-23 20:31:22 -0800

    Initial commit

---
+++ input_output_prac.py
@@ -0,0 +1,8 @@
+num1 = raw_input("Enter a number: ")
+num1 = int(num1)
+num2 = raw_input("Enter another number: ")
+num2 = int(num2)
+print "num1 + num2 = " + str(num1+num2)
+print "num1 - num2 = " + str(num1-num2)
+print "num1 * num2 = " + str(num1*num2)
+print "num1 / num2 = " + str(num1/num2)
